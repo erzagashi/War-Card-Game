b3e8a7fa9a09d47cd6cf3be4e29d09cefce78d48
erzagashi <37579167+erzagashi@users.noreply.github.com>
2018-05-26 17:23:58 +0200

    Add files via upload

---
+++ WarGame.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class WarGame {
+public static void main(String[] args) {
+
+    Card[][] hands = new Card[2][1];
+    Deck myDeck = new Deck();
+
+    for (int i = 1; i <=26; i++) {
+        System.out.printf("\n Round %s of The War \n", i);
+
+        
+            for (int player = 0; player < hands.length; player++)
+                hands[player][0] = myDeck.dealCard();
+
+        for (int player = 0; player < hands.length; player++) {
+            System.out.printf("Player %d: ", player);
+            printHand(hands[player]);
+        }
+
+        int player1 = hands[0][0].getValue(); 
+        int player2 = hands[1][0].getValue();
+
+        if (player1 > player2)
+            System.out.println("Player One Wins The War");
+        else if (player2 > player1)
+            System.out.println("Player Two Wins The War");
+        else
+            System.out.println("The War Is A Tie");
+
+    }
+}
+ public static void printHand(Card[] hand) {
+
+        for (int card = 0; card < hand.length; card++)
+            System.out.printf("%s", hand[card].toString());
+
+        System.out.println();
+
+    } 
+}
